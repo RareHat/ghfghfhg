a4afd930f0689ee3686e2d0cfafd8cf33f7adfd7
meimeuiii <141917559+vadimzhenchur@users.noreply.github.com>
2024-10-27 16:27:28 +0200

    12412
    214124

--- ggjggj.py
+++ ggjggj.py
@@ -1,5 +1,5 @@
 import os
-from PIL import Image
+from PIL import Image, ImageFilter
 from PyQt6.QtGui import QImage, QPixmap
 from PyQt6.QtWidgets import *
 
@@ -38,7 +38,37 @@ class PhotoManager:
         image_lbl.setPixmap(pixels)
 
 
+    def bw(self):
+        self.photo = self.photo.convert("L")
+        self.show_image(self.image_lbl)
 
+    def sharp(self):
+        self.photo = self.filter(ImageFilter.SHARPEN)
+        self.show_image(self.image_lbl)
+
+    def right(self):
+        self.photo = self.photo.transpose(Image.ROTATE_90)
+        self.show_image(self.image_lbl)
+
+    def kontur(self):
+        self.photo = self.filter(ImageFilter.CONTOUR)
+        self.show_image(self.image_lbl)
+
+    def flip(self):
+        self.photo = self.transpose(Image.FLIP_LEFT_RIGHT)
+        self.show_image(self.image_lbl)
+
+    def Edge(self):
+        self.photo = self.filter(ImageFilter.EDGE_ENHANCE)
+        self.show_image(self.image_lbl)
+
+    def Bright(self):
+        self.photo = self.ImageEnhance.Brightness(image_lbl).enhance(1.5)
+        self.show_image(self.image_lbl)
+
+    def blur(self):
+        self.photo = self.ImageEnhance.Brightness().enhance(1.5)
+        self.show_image(self.image_lbl)
 
 
 
@@ -50,11 +80,14 @@ menu_btn = QPushButton("Меню")
 
 line_lbl = QLabel("Картинка")
 list_widg = QListWidget()
-button_btn = QPushButton("Вліво")
-button2_btn = QPushButton("Вправо")
+button_btn = QPushButton("Яскравість 50%")
+button2_btn = QPushButton("Розмивання")
 button3_btn = QPushButton("Дзеркало")
-button4_btn = QPushButton("Рідкість")
+button4_btn = QPushButton("Яскравість 50%")
 button5_btn = QPushButton("Ч/Б")
+button6_btn = QPushButton("Накладання контурів")
+button7_btn = QPushButton("Збільшення різкості")
+button8_btn = QPushButton("Покращення ребер")
 buttonv2_btn = QPushButton("Папка")
 
 main_line = QHBoxLayout()
@@ -76,7 +109,7 @@ v2.addWidget(button5_btn)
 v3.addLayout(v2)
 
 photo_manager = PhotoManager()
-
+photo_manager.image_lbl = line_lbl
 
 def open_folder():
     photo_manager.folder = QFileDialog.getExistingDirectory()
@@ -93,6 +126,14 @@ list_widg.currentRowChanged.connect(show_chosen_image)
 
 
 buttonv2_btn.clicked.connect(open_folder)
+button5_btn.clicked.connect(photo_manager.bw)
+button7_btn.clicked.connect(photo_manager.sharp())
+button8_btn.clicked.connect(photo_manager.Edge)
+button4_btn.clicked.connect(photo_manager.Bright())
+button5_btn.clicked.connect(photo_manager.blur())
+button3_btn.clicked.connect(photo_manager.flip)
+button6_btn.clicked.connect(photo_manager.kontur)
+button5_btn.clicked.connect(photo_manager.right)
 
 main_line.addLayout(v3)
 
